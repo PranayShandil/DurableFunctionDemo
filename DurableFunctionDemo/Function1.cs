b97a81045c776860acd01e89166e437085f3db57
Pranay Shandil <Pranay.Shandil@xlent.se>
2024-09-11 14:42:06 +0200

    Add stockholm

--- DurableFunctionDemo/Function1.cs
+++ DurableFunctionDemo/Function1.cs
@@ -24,6 +24,7 @@ namespace DurableFunctionDemo
                 outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Tokyo"));
                 outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Seattle"));
                 outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "London"));
+                outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Stockholm"));
             }
             catch (Exception ex)
             {
